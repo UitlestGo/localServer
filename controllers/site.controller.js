01844068ad0148ae47b7ff67bb5024a0d296f0a3
An Khang <anhkhanguit@gmail.com>
2016-09-13 21:29:52 +0700

    first commit

---
+++ controllers/site.controller.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('config.json');
+var tokenService = require('services/token.service');
+
+
+
+router.post('/login', function (req, res) {
+    // authenticate using api to maintain clean separation between layers
+    request.post({
+        url: config.apiUrl + '/users/authenticate',
+        form: req.body,
+        json: true
+    }, function (error, response, body) {
+        if (error) {
+            return res.status(401).json({'message':'An error occurred' , 'successful' : 'false', 'info' : ''});
+        }
+
+        if (!body.message) {
+            return res.status(401).json({'message':'Username or password is incorrect' , 'successful' : 'false', 'info' : ''});
+        }
+
+        res.status(200).json(body.message);
+    });
+});
+
+router.post('/register', function (req, res) {
+    // register using api to maintain clean separation between layers
+    request.post({
+        url: config.apiUrl + '/users/register',
+        form: req.body,
+        json: true
+    }, function (error, response, body) {
+        if (error) {
+            return res.status(401).json({'message':'An error occurred' , 'successful' : 'false', 'info' : ''});
+        }
+
+        if (response.statusCode !== 200) {
+            return res.status(401).json({'message':response.body , 'successful' : 'false', 'info' : ''});
+        }
+
+        // return success message
+        res.status(401).json({'message':'Register successful' , 'successful' : 'true', 'info' : ''});
+    });
+});
+
+router.post('/logout', function (req, res) {
+    // register using api to maintain clean separation between layers
+    if(req.body.author && req.body.token) {
+        tokenService.delete(req.body.author, req.body.token).then(function (success) {
+                if(success){
+                    return res.status(200).json({'message':'' , 'successful' : 'true', 'info' : ''});
+                }
+            })
+            .catch(function (err) {
+                return res.status(401).json({'message':'token no match' , 'successful' : 'false', 'info' : ''});
+            });
+    }else{
+        res.status(401).json({'message':'validate error' , 'successful' : 'false', 'info' : ''});
+    }
+});
+
+router.post('/authenticate', function (req, res) {
+    // register using api to maintain clean separation between layers
+    if(req.body.token) {
+        tokenService.find(req.body.token).then(function (success) {
+                if(success){
+                    return res.status(200).json({'message':'' , 'successful' : 'true', 'info' : ''});
+                }
+
+            })
+            .catch(function (err) {
+                return res.status(401).json({'message':'token no match' , 'successful' : 'false', 'info' : ''});
+            });
+    }else{
+        res.status(401).json({'message':'validate error' , 'successful' : 'false', 'info' : ''});
+    }
+});
+
+
+
+
+module.exports = router;
---
+++ services/token.service.js
@@ -0,0 +1,94 @@
+var config = require('config.json');
+var _ = require('lodash');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('token');
+
+var service = {};
+
+service.authenticate = authenticate;
+service.create = create;
+service.delete = _delete;
+service.find = findToken;
+
+module.exports = service;
+
+function authenticate(token) {
+    var deferred = Q.defer();
+
+    db.token.findOne({ token: token }, function (err, tokenMsg) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (tokenMsg) {
+            // authentication successful
+            deferred.resolve(tokenMsg);
+        } else {
+            // authentication failed
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function create(author, token) {
+    var deferred = Q.defer();
+    // validation
+    db.token.findOne(
+        { token: token },
+        function (err, token) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (token) {
+                // username already exists
+                deferred.reject('Token ' + token + ' is already taken');
+            } else {
+                createToken();
+            }
+        });
+
+    function createToken() {
+        var objInsert = {'author': author, 'token' : token};
+        db.token.insert(
+            objInsert,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function _delete(author, token) {
+    var deferred = Q.defer();
+
+    db.token.remove(
+        {author: author, token: token},
+        function (err) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+            deferred.resolve('Token ' + token + ' match');
+        });
+
+    return deferred.promise;
+}
+
+function findToken(token){
+    var deferred = Q.defer();
+    // validation
+    db.token.findOne(
+        { token: token },
+        function (err, token) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (token) {
+                // username already exists
+                deferred.resolve('Token ' + token + ' match');
+            } else {
+                deferred.reject('Token ' + token + ' no match');
+            }
+        });
+    return deferred.promise;
+}
